004896324026915726a16842680aac6af7de1634
t-hnh10 <t.hnh@tuta.io>
2019-04-03 16:05:58 +1000

    added hashes_beast

---
+++ hashes/hashes_beast.rb
@@ -0,0 +1,25 @@
+# Beast Mode: Lunch Order Splitting
+
+# Initialisation
+amount_owed = []
+
+# Hash
+items = [{ customer: "John", item: "Soup", cost: 8.50},
+{customer: "Sarah", item: "Pasta", cost: 12},
+{customer: "John", item: "Coke", cost: 2.50}]
+
+puts "Welcome! Please enter a customer's name."
+customer_name = gets.chomp.capitalize
+
+items.each do |customer_search|
+    if customer_search[:customer] == customer_name
+        amount_owed.push(customer_search[:cost])
+    end
+end
+
+total_owed = '%.2f' % amount_owed.sum
+puts "#{customer_name} owes $#{total_owed}"
+
+# Calculating total owed
+# items.each do |total_owed|
+#    if amount_owed
