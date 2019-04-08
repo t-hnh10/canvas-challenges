413e7d7957ec63f611238e9a76d848fd859c8d2f
t-hnh10 <t.hnh@tuta.io>
2019-04-08 13:55:26 +1000

    implemeneted add method in methods

--- methods/methods.rb
+++ methods/methods.rb
@@ -4,3 +4,21 @@ end
 
 say('Hello')
 say('Welcome to my application!')
+
+def add(n1,n2,n3)
+    if
+        (n1.is_a?(Numeric) and n2.is_a?(Numeric) and n3.is_a?(Numeric))
+    else
+        puts "Invalid input"
+    end
+end
+
+puts add(1,2,3) # => 6
+puts add("1",2,3)
+puts add("one",2,3)
+
+# Handles strings
+=begin def add(n1,n2,n3)
+    return n1.to_i+n2.to_i+n3.to_i
+end
+=end
